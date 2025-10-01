24968d39523b77c6e6e172277d303e717a9a6816
Alpha <148535861+alphay78@users.noreply.github.com>
2025-10-01 14:37:31 +0300

    views.py , urls.py

---
+++ Django-signals_orm-0x04/messaging/views.py
@@ -0,0 +1,17 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
+from django.http import HttpResponse
+
+User = get_user_model()
+
+@login_required
+def delete_user(request):
+    """
+    Allow a logged-in user to delete their account.
+    Triggers the post_delete signal to clean up related data.
+    """
+    user = request.user
+    username = user.username
+    user.delete()
+    return HttpResponse(f"User {username} and all related data have been deleted.")
